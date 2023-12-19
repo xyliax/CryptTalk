2d3a8680222b22388b1c915143296a45b52e4d97
jason <jasonlyuchina@gmail.com>
2023-12-19 20:35:55 +0800

    update

---
+++ src/security/ClientTest.py
@@ -0,0 +1,60 @@
+from socket import *
+from Safe_Part import *
+import time
+def send_string(client, message):
+  client.send(bytes(message, encoding='utf-8'))
+  time.sleep(3)
+  
+def recv_string(client):
+  return client.recv(2100).decode('utf-8')
+
+def send_byte(client,message) :
+  client.send(message)
+  time.sleep(3)
+
+def recv_byte(client) :
+  return client.recv(2100)
+
+def Register(client,ServerPublicKey,username,password,email) :
+   
+    username=encrypt_with_public_key(ServerPublicKey,username)
+    send_byte(client,username)
+    password=encrypt_with_public_key(ServerPublicKey,password)
+    send_byte(client,password)
+    email=encrypt_with_public_key(ServerPublicKey,email)
+    send_byte(client,email)
+
+def Login(client,myPrivatekey,ServerPublicKey,username,password) :
+    username=encrypt_with_public_key(ServerPublicKey,username)
+    send_byte(client,username)
+    password=encrypt_with_public_key(ServerPublicKey,password)
+    send_byte(client,password)
+    email=recv_byte(client)
+    email=decrypt_with_private_key(myPrivatekey,email)
+    return email
+
+def Keyexchange(myPublickey,Client) :
+    myPublickey,myPrivatekey=generate_key_pair()
+    string=recv_string(Client)
+    if string[0:5]=='prime' :
+        prime=get_numbers(string)
+    string=recv_string(Client)
+    if string[0:9]=='generator' :
+        generator=get_numbers(string)
+    string=recv_string(Client)
+    if string[0:6]=='public' :
+        heDiffieHellmanPublicKey=get_numbers(string)
+    myDiffieHellmanPrivatekey = getgenerator_SK(prime)
+    myDiffieHellmanPublickey = calculate_public_key(prime, generator, myDiffieHellmanPrivatekey)
+    send_string(Client,"public key "+str(myDiffieHellmanPublickey))
+    sharedSecret=calculate_shared_secret(prime, heDiffieHellmanPublicKey, myDiffieHellmanPrivatekey)
+    encrypted_publicKey=encrypt_with_aes(myPublickey.save_pkcs1().decode(),str(sharedSecret))
+    send_byte(Client,encrypted_publicKey)
+    ServerPublicKey=rsa.PublicKey.load_pkcs1((decrypt_with_aes(recv_byte(Client),str(sharedSecret))).encode())
+
+    return myPrivatekey,myPublickey,ServerPublicKey
+def clientSocket( ) :
+    Client = socket(AF_INET,SOCK_STREAM)
+    Client.connect(('127.0.0.1',8888))
+    return Client
+
